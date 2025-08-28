4389d06bee48b94f159a2ad7e4456890617785d2
Ekaterina <zhukova.ekaterina.1988@gmail.com>
2025-08-28 14:38:39 +0300

    add task from lesson 3.6 step 10

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+def test_check_button_add_to_basket_is_exist(browser):
+    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    add_to_basket_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert add_to_basket_button.is_displayed(), "Button is not displayed"
